46711ce2406a1ec55b594603d599d6a2feb9d06d
LocalPhilosopher <rekunigato@gmail.com>
2024-04-14 14:19:42 +0300

    - add recipe

--- Assets/Code/UI/RecipeUI.cs
+++ Assets/Code/UI/RecipeUI.cs
@@ -1,42 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Code.Core.Bestiary;
+using Code.Core.Likes;
+using Code.Core.Pentagram;
+using TMPro;
 
 namespace Code.UI.Book
 {
     public class RecipeUI : MonoBehaviour
     {
+        [SerializeField] private Button buyBtn;
+        [SerializeField] private TextMeshProUGUI price;
         [SerializeField]
         Recipe recipe;
 
         [SerializeField]
         List<Image> images;
 
+
+        private void OnEnable()
+        {
+
+            buyBtn.onClick.AddListener(Buy);
+            if (recipe)
+                CheckRecipeToShow();
+        }
+
+        private void OnDisable()
+        {
+            
+            buyBtn.onClick.RemoveAllListeners();
+        }
+        
         public void SetUp(Recipe rec)
         {
             recipe = rec;
             CheckRecipeToShow();
         }
 
-        private void OnEnable()
+        public void Buy()
         {
-            if (recipe)
+            if (LikesSystem.Likes >= recipe.price)
+            {
+                LikesSystem.Likes -= recipe.price;
+                BestiaryBook.CollectRecipe(recipe);
                 CheckRecipeToShow();
+            }
         }
-
         void CheckRecipeToShow()
         {
             if (BestiaryBook.CheckRecipe(recipe))
             {
+                buyBtn.gameObject.SetActive(false);
                 foreach (Image item in images)
                 {
                     item.sprite = recipe.ingredients[images.IndexOf(item)].Icon;
                 }
             }
+            else
+            {
+                price.text = recipe.price.ToString();
+                buyBtn.gameObject.SetActive(true);
+            }
         }
 
+        // private void Update()
+        // {
+        //     if (Input.GetKeyDown(KeyCode.A))
+        //         LikesSystem.Likes++;
+        // }
     }
 
 }
