09d76e6bca94b92f779cf7eab3dc40178dce47fe
LocalPhilosopher <rekunigato@gmail.com>
2024-04-14 15:39:15 +0300

    - add start screen

---
+++ Assets/Code/UI/StartScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class StartScreen : MonoBehaviour
+    {
+        [SerializeField] private Button startBtn;
+        [SerializeField] private CanvasGroup canvas;
+        private bool gameStarted;
+
+        private void Awake()
+        {
+            startBtn.onClick.AddListener(StartGame);
+        }
+
+        void StartGame()
+        {
+            if (gameStarted)
+                return;
+            gameStarted = true;
+            DOTween.To(() => canvas.alpha, x => canvas.alpha = x, 0, 1f).OnComplete(() =>
+            {
+                gameObject.SetActive(false);
+            });
+        }
+    }
+}
